4e71ef44cde03bf5e684219b8322844e2a5df751
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-19 03:03:13 +0100

    Created a postgresql account and users table. Added express and morgan middleware and a simple test route /

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -4,7 +4,7 @@
 
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
-@Entity()
+@Entity('users')
 export class User {
 
     @PrimaryGeneratedColumn()
--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,25 @@
 
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entities/User";
+import express from 'express'
+import morgan from "morgan";
 
-// Here we are generally initiating our database connection and adding our database entities
+const app = express()
 
-createConnection().then(async connection => {
+app.use(express.json())
+app.use(morgan('dev'))
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+app.get('/',  (req,res) => {
+    res.send("hello world")
+})
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+app.listen(5000, async() => {
+    console.log("Server running at http://localhost:5000");
+    
+    try{
+        await createConnection()
+        console.log('Database connected');
+    } catch(err){
+    console.log(err);
+    }
+})
