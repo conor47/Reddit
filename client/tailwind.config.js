0f51a8055550dd28fb299b14cf94e52afd172cc4
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-20 21:04:36 +0100

    Continued development of register page markup and css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: "17.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
