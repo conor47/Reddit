7c4fd31458e008379525d233389eafe5912166bc
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-20 23:53:20 +0100

    Improved error messaging for register endpoint and added extra constraints to User entity. Custom function for mapping typeorm error object to better formatted object.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,114 +1,124 @@
 import { Request, Response, Router } from "express";
 import { validate, isEmpty } from "class-validator";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import cookie from 'cookie'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
-import  User  from "../entities/User";
-import auth from "../middleware/auth"
+import User from "../entities/User";
+import auth from "../middleware/auth";
+
+// this function is to format our errors better. Without this the typeorm errors returned are more
+// difficult to parse. Now we can easily access the errors for displaying on the frontend
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
 
 // register route for handling POST requests to /api/auth/register
 const register = async (req: Request, res: Response) => {
-    const {email, username, password} = req.body
+  const { email, username, password } = req.body;
 
-    try{
-        // Validate data
-        let errors:any = {}
-        const emailUser = await User.findOne({email})
-        const usernameUser = await User.findOne({username})
+  try {
+    // Validate data
+    let errors: any = {};
+    const emailUser = await User.findOne({ email });
+    const usernameUser = await User.findOne({ username });
 
-        if(emailUser) errors.email = "Email is already taken"
-        if(usernameUser) errors.username = "Username is already taken"
+    if (emailUser) errors.email = "Email is already taken";
+    if (usernameUser) errors.username = "Username is already taken";
 
-        if(Object.keys(errors).length > 0) {
-            return res.status(400).json(errors)
-        }
-
-        // Create the user
-        const user = new User({email, password, username})
-
-        errors = await validate(user)
-        if(errors.length > 0) return res.status(400).json({errors})
-
-        await user.save()
-
-        // Return the user
-        return res.json(user)
-    } catch (err){
-        console.log(err);
-        return res.status(500).json(err)
-        
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
     }
-}
+
+    // Create the user
+    const user = new User({ email, password, username });
+
+    errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
+    }
+
+    await user.save();
+
+    // Return the user
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
 
 // login route for handling POST requests to /api/auth/login
-const login = async (req : Request, res : Response) => {
-    const {username, password} = req.body
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
-    try {
-        let errors : any = {}
+  try {
+    let errors: any = {};
 
-        if(isEmpty(username)) errors.username = "Username must not be empty"
-        if(isEmpty(password)) errors.password = "Password must not be empty"
-        if(Object.keys(errors).length > 0) return res.status(400).json(errors)
-        const user = await User.findOne({username})
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+    const user = await User.findOne({ username });
 
-        if(!user) return res.status(404).json({error : "User not found"})
+    if (!user) return res.status(404).json({ error: "User not found" });
 
-        const passwordMatches = await bcrypt.compare(password, user.password)
-        if(!passwordMatches){
-            return res.status(401).json({password : "password is incorrect"})
-        }
-
-        const token = jwt.sign({username}, process.env.JWT_SECRET!)
-
-        // res.set is used to set response headers. We are setting the Set-Cookie header which is used to send
-        // a cookie from the server to the client so that the client can store it and send it back later.
-        res.set('Set-Cookie', cookie.serialize('token', token, {
-            httpOnly:true,
-            secure: process.env.NODE_ENV ===  'production',
-            sameSite:'strict',
-            maxAge:3600,
-            path:'/'
-        }))
-
-        return res.json(user)
-
-    } catch (error) {
-        console.log(error);
-        return res.json({error: "Something went wrong"})
-        
+    const passwordMatches = await bcrypt.compare(password, user.password);
+    if (!passwordMatches) {
+      return res.status(401).json({ password: "password is incorrect" });
     }
-}
 
-// route for testing whether a user is logged in 
-const me = async (_ : Request, res : Response) => {
-  return res.json(res.locals.user)
-}
+    const token = jwt.sign({ username }, process.env.JWT_SECRET!);
+
+    // res.set is used to set response headers. We are setting the Set-Cookie header which is used to send
+    // a cookie from the server to the client so that the client can store it and send it back later.
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 3600,
+        path: "/",
+      })
+    );
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: "Something went wrong" });
+  }
+};
+
+// route for testing whether a user is logged in
+const me = async (_: Request, res: Response) => {
+  return res.json(res.locals.user);
+};
 
 // a route for logging a user out
-const logout = (_ : Request, res : Response) => {
+const logout = (_: Request, res: Response) => {
+  // to remove a cooke we cannot simply request that the client deletes the cookie. We essentially override
+  // the current cookie with a new cookie that expires immediately. When the cookie expires the browser will delete it
+  res.set(
+    "Set-Cookie",
+    cookie.serialize("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: new Date(0),
+      path: "/",
+    })
+  );
 
-    // to remove a cooke we cannot simply request that the client deletes the cookie. We essentially override
-    // the current cookie with a new cookie that expires immediately. When the cookie expires the browser will delete it
-    res.set('Set-Cookie', cookie.serialize('token', '', {
-        httpOnly:true,
-            secure: process.env.NODE_ENV ===  'production',
-            sameSite:'strict',
-            expires: new Date(0),
-            path:'/'
-    }))
+  return res.status(200).json({ success: true });
+};
 
-    return res.status(200).json({success:true})
-    
-}
+const router = Router();
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", auth, me);
+router.get("/logout", auth, logout);
 
-
-
-const router = Router()
-router.post('/register', register)
-router.post('/login', login)
-router.get('/me',auth, me)
-router.get('/logout',auth, logout)
-
-export default router
+export default router;
