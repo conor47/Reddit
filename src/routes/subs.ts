927b6248865bf73228b410e461c6bba82e7c6ce5
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-20 05:40:27 +0100

    Begun development of the sub router. Wrote the first sub endpoint for creating new subs.

---
+++ src/routes/subs.ts
@@ -0,0 +1,56 @@
+import { Request, Response, Router } from "express";
+import { IsEmpty } from "class-validator";
+import { getRepository } from "typeorm";
+
+import User from "../entities/User";
+import Sub from "../entities/Sub";
+import auth from "../middleware/auth";
+
+const createSub = async (req : Request, res : Response) => {
+    const {name, title, description} = req.body
+
+    const user : User = res.locals.user
+
+    try {
+        let errors: any = {}
+
+        if(IsEmpty(name)) errors.name = "Name must not be empty"
+        if(IsEmpty(title)) errors.title = "Title must not be empty"
+
+        const sub = await getRepository(Sub)
+        .createQueryBuilder('sub')
+        // the : indicates that we are going to pass a variable. we passed the name variable
+        .where('lower(sub.name) = :name', {name: name.toLowerCase()})
+        .getOne()
+
+        if(sub) errors.name = "Sub exists already"
+
+        if(Object.keys(errors).length > 0){
+            throw errors
+        }
+        
+    } catch (err) {
+        return res.status(400).json(err)
+    }
+
+    try {
+
+        // if we reach this point then everything is valid and we want to create and persist the sub
+        
+        const sub = new Sub({name, description, title, user})
+        await sub.save()
+
+        return res.json(sub)
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error : "Something went wrong"})
+        
+    }
+}
+
+const router = Router()
+
+router.post('/',auth, createSub)
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ dotenv.config()
 
 import authRoutes from './routes/auth'
 import postRoutes from './routes/posts'
+import subRoutes from './routes/subs'
 import trim from'./middleware/trim'
 
 const app = express()
@@ -27,6 +28,7 @@ app.get('/',  (_,res) => {
 })
 app.use('/api/auth', authRoutes)
 app.use('/api/posts', postRoutes)
+app.use('/api/subs', subRoutes)
 
 app.listen(PORT, async() => {
     console.log(`Server running at http://localhost:${PORT}`);
