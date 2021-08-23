867587af9c879c96b96f9005af0b285ee9b1a0a8
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-24 00:09:41 +0100

    Created misc routes. Begin working on vote route for handling voting logic on post and comments

---
+++ src/routes/misc.ts
@@ -0,0 +1,53 @@
+// collection of miscellaneous routes
+import { Router } from "express";
+import { Request, Response } from "express-serve-static-core";
+
+import Comment from "../entities/Comment";
+import Post from "../entities/Post";
+import User from "../entities/User";
+import Vote from "../entities/Vote";
+import auth from "../middleware/auth";
+
+const router = Router();
+
+const vote = async (req: Request, res: Response) => {
+  const { identifier, slug, commentIdentifier, value } = req.body;
+
+  // todo Validate vote value
+  if (![-1, 0, 1].includes(value)) {
+    return res.status(400).json({ value: "value must be -1, 0 or 1" });
+  }
+  try {
+    const user: User = res.locals.user;
+    let post = await Post.findOneOrFail({ identifier, slug });
+    let vote: Vote | undefined;
+    let comment: Comment | undefined;
+
+    if (commentIdentifier) {
+      // If there is a comment identifier, find a vote by comment
+      comment = await Comment.findOneOrFail({ identifier: commentIdentifier });
+      vote = await Vote.findOne({ user, comment });
+    } else {
+      // Else find vote by this user on this post
+      vote = await Vote.findOne({ user, post });
+    }
+
+    if (!vote && value === 0) {
+      // if no vote and value is 0 return error
+      return res.status(404).json({ error: "vote not found" });
+    } else if (!vote) {
+      // if we reach here we know that we have a value but have not voted yet , this we create the vote
+      // and add the comment if its a vote on a comment or add the post if its a vote on a post
+      vote = new Vote({ user, value });
+      if (comment) vote.comment = comment;
+      else vote.post = post;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
+
+router.post("/vote", auth, vote);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ dotenv.config();
 import authRoutes from "./routes/auth";
 import postRoutes from "./routes/posts";
 import subRoutes from "./routes/subs";
+import miscRoutes from "./routes/misc";
 import trim from "./middleware/trim";
 
 const app = express();
@@ -37,6 +38,7 @@ app.get("/", (_, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/subs", subRoutes);
+app.use("/api/misc", miscRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Server running at http://localhost:${PORT}`);
