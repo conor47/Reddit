0ec4d152e16c54409486e5bf4c0c52b237ac5682
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-24 18:08:09 +0100

    Refacrtored posts into their own component. Also refactored action buttons on posts into their own component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,6 +9,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { Post } from "../types";
+import PostCard from "../components/PostCard";
 
 dayjs.extend(relativeTime);
 
@@ -36,67 +37,7 @@ export default function Home() {
         {/* Posts feed */}
         <div className="w-160">
           {posts.map((post) => (
-            <div key={post.identifier} className="flex mb-4 bg-white rounded">
-              {/* vote section */}
-              <div className="w-10 text-center bg-gray-200 rounded-l">
-                <p>V</p>
-              </div>
-              {/* post data section */}
-              <div className="w-full p-2">
-                <div className="flex items-center">
-                  <Link href={`/r/${post.subName}`}>
-                    <Fragment>
-                      <img
-                        src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
-                        alt="gravatar"
-                        className="h-6 mr-1 rounded-full cursor-pointer"
-                      />
-                      <a className="text-xs font-bold cursor-pointer hover:underline">
-                        /r/{post.subName}
-                      </a>
-                    </Fragment>
-                  </Link>
-                  <p className="text-gray-500 text-excess">
-                    <span className="mx-1">•</span>
-                    Posted by
-                    <Link href={`/u/${post.username}`}>
-                      <a href="" className="mx-1 hover:underline">
-                        /u/{post.username}
-                      </a>
-                    </Link>
-                    <Link
-                      href={`/r/${post.subName}/${post.identifier}/${post.slug}`}
-                    >
-                      <a href="" className="mx-1 hover:underline">
-                        {dayjs(post.createdAt).fromNow()}
-                      </a>
-                    </Link>
-                  </p>
-                </div>
-                <Link href={post.url}>
-                  <a className="my-1 text-lg font-medium">{post.title}</a>
-                </Link>
-                {post.body && <p className="my-1 text-sm">{post.body}</p>}
-                <div className="flex">
-                  <Link href={post.url}>
-                    <a>
-                      <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                        <span className="font-bold">20 comments</span>
-                      </div>
-                    </a>
-                  </Link>
-                  <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-bookmark fa-xs"></i>
-                    <span className="font-bold">Share</span>
-                  </div>
-                  <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-share fa-xs"></i>
-                    <span className="font-bold">Save</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PostCard post={post} key={post.identifier} />
           ))}
         </div>
       </div>
