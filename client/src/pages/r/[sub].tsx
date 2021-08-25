89fa145edf5e42fa542cb28abd6fd7f0bb3db069
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-26 00:45:47 +0100

    Continued development of sub page.

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,13 +1,20 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { Fragment } from "react";
+import {  Fragment, useRef } from "react";
 import useSWR from "swr";
 import Image from "next/image";
+import classnames from 'classnames'
 
 import PostCard from "../../components/PostCard";
 import { Sub } from "../../types";
+import {useAuthState} from "../../context/auth"
 
 export default function SubName() {
+  // Local state
+  // Global state
+  const {authenticated,user} = useAuthState()
+  // Utils
+  const fileInputRef = useRef()
   const router = useRouter();
 
   const subName = router.query.sub;
@@ -38,6 +45,7 @@ export default function SubName() {
       </Head>
       {sub && (
         <Fragment>
+          input type="file" hidden={true} ref={fileInputRef} />
           {/* sub info and images */}
           <div>
             {/* Banner image */}
@@ -58,14 +66,24 @@ export default function SubName() {
             </div>
             {/* Sub meta data */}
             <div className="h-20 bg-white">
-              <div className="container flex">
-                <Image
-                  src={sub.imageUrl}
-                  alt="Sub"
-                  className="rounded-full"
-                  width={80}
-                  height={80}
-                />
+              <div className="container relative flex">
+                <div className="absolute" style={{ top: -15 }}>
+                  <Image
+                    src={sub.imageUrl}
+                    alt="Sub"
+                    className="rounded-full"
+                    width={70}
+                    height={70}
+                  />
+                </div>
+                <div className="pt-1 pl-24">
+                  <div className="flex items center">
+                    <h1 className="mb-1 text-3xl font-bold">{sub.title}</h1>
+                  </div>
+                  <p className="font-bold text-gray-500 text-small">
+                    /r/{sub.name}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
