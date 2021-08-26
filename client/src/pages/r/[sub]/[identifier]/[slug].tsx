51819bd2714c1c13f7ef18544b2930c95d362717
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-26 05:35:59 +0100

    Continued development of individual post page.

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,12 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import Image from "next/image";
+
+import { Post } from "../../../../types";
+import Sidebar from "../../../../components/Sidebar";
 
 export default function PostPage() {
   const router = useRouter();
   const { identifier, sub, slug } = router.query;
 
-  const { data: post, error } = useSWR(
+  const { data: post, error } = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null
   );
   if (error) {
@@ -17,6 +22,31 @@ export default function PostPage() {
       <Head>
         <title>{post?.title}</title>
       </Head>
+      <Link href={`/r/${sub}`}>
+        <a>
+          <div className="flex items-center w-full h-20 p-8 bg-blue-500">
+            <div className="container flex">
+              {post && (
+                <div className="w-8 h-8 mr-2 overflow-hidden rounded-full">
+                  <Image
+                    src={post.sub.imageUrl}
+                    height={(8 * 16) / 4}
+                    width={(8 * 16) / 4}
+                    alt="Image"
+                  />
+                </div>
+              )}
+              <p className="text-xl font-semibold text-white">/r/{sub}</p>
+            </div>
+          </div>
+        </a>
+      </Link>
+      <div className="container flex pt-5">
+        {/* Post */}
+        <div className="w-160"></div>
+        {/* Sidebar */}
+        {post && <Sidebar sub={post.sub} />}
+      </div>
     </>
   );
 }
--- client/src/types.ts
+++ client/src/types.ts
@@ -7,6 +7,7 @@ export interface Post {
   updatedAt: string;
   body?: string;
   username: string;
+  sub?: Sub;
   //   virtual fields
   url: string;
   voteScore?: number;
