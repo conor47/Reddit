76c7dece2fe069768bda869e10121af94a05e284
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-08-26 04:03:43 +0100

    Begun craetion of sidebar. Displaying top 5 subreddits by posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @next/next/link-passhref */
 /* eslint-disable @next/next/no-img-element */
-import axios from "axios";
 import Head from "next/head";
-import Link from "next/link";
 import { GetServerSideProps } from "next";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import useSWR from "swr";
+import Image from "next/image";
 
-import { Post } from "../types";
+import { Sub } from "../types";
 import PostCard from "../components/PostCard";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -31,6 +31,7 @@ export default function Home() {
   //     .catch((err) => console.log(err));
   // }, []);
   const { data: posts } = useSWR("/posts");
+  const { data: topSubs } = useSWR("/misc/top-subs");
 
   return (
     <Fragment>
@@ -43,6 +44,41 @@ export default function Home() {
           {posts?.map((post) => (
             <PostCard post={post} key={post.identifier} />
           ))}
+        </div>
+        {/* Sidebar */}
+        <div className="ml-6 w-80">
+          <div className="bg-white rounded">
+            <div className="p-4 border-b-2">
+              <p className="text-lg font-semibold text-center">
+                Top Communities
+              </p>
+            </div>
+            <div>
+              {topSubs?.map((sub: Sub) => (
+                <div
+                  key={sub.name}
+                  className="flex items-center px-4 py-2 text-xs border-b"
+                >
+                  <div className="mr-2 overflow-hidden rounded-full cursor-pointer">
+                    <Link href={`/r/${sub.name}`}>
+                      <Image
+                        src={sub.imageUrl}
+                        alt="Sub"
+                        width={(6 * 16) / 4}
+                        height={(6 * 16) / 4}
+                      />
+                    </Link>
+                  </div>
+                  <Link href={`/r/${sub.name}`}>
+                    <a className="font-bold hover:cursor-pointer">
+                      /r/{sub.name}
+                    </a>
+                  </Link>
+                  <p className="ml-auto font-medium">{sub.postCount}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
