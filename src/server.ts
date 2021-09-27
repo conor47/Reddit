130d073fec03d54cccee040b88ae8e83a88f3a53
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-09-27 15:26:31 +0100

    trying to fix cors error

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ import trim from './middleware/trim';
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(cors({ origin: 'http://139.59.168.190', credentials: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
